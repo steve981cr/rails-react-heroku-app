d50ad47092a9e90d4d7fc6c548aaedc1c3bee385
Steve Carey <steve981cr@gmail.com>
2019-07-02 16:45:00 -0700

    minor change

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './stylesheets/index.css';
 import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+// serviceWorker.unregister();
